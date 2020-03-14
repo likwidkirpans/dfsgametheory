1b21de3fabf6a15e9af9dffe237decde0c414228
likwidkirpans <2270977+likwidkirpans@users.noreply.github.com>
2020-03-14 17:07:58 -0400

    updated example file

--- example.py
+++ example.py
@@ -1,8 +1,17 @@
-from pydfs_lineup_optimizer import Site, Sport, get_optimizer
+import time
+start_time = time.time()
 
+from pydfs_lineup_optimizer import get_optimizer, Site, Sport, CSVLineupExporter
 
-optimizer = get_optimizer(Site.YAHOO, Sport.BASKETBALL)
-optimizer.load_players_from_csv("yahoo-NBA.csv")
-lineup_generator = optimizer.optimize(10)
-for lineup in lineup_generator:
-    print(lineup)
+optimizer = get_optimizer(Site.FANDUEL, Sport.BASKETBALL)
+optimizer.load_players_from_csv("fd.csv")
+
+optimizer.set_min_salary_cap(59400)
+
+exporter = CSVLineupExporter(optimizer.optimize(n=1000, randomness=True))
+exporter.export('result.csv')
+
+#for lineup in optimizer.optimize(n=150, randomness=True):
+    #print("-")
+
+print("--- %s seconds ---" % (time.time() - start_time))
